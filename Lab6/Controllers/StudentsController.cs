75ed7d4794b7f3785d6c3b6701aefd7156819787
Christopher Decariedawson <ChristopherMCDawson@gmail.com>
2023-07-31 15:06:40 -0400

    Add project files.

---
+++ Lab6/Controllers/StudentsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Lab6.Data;
+using Lab6.Models;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace Lab6.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly StudentDbContext _context;
+
+        public StudentsController(StudentDbContext context)
+        {
+            _context = context;
+        }
+
+        /*For each action, I've added ProducesResponseType
+         * annotations to specify the
+         * possible HTTP response types and their
+         * corresponding data types.
+         */
+
+
+        // GET: api/Students
+        [HttpGet]
+        // For GetStudents, it returns a list of Student objects with a 200 OK response.
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<Student>))]
+        public async Task<ActionResult<IEnumerable<Student>>> GetStudents()
+        {
+            return await _context.Students.ToListAsync();
+        }
+
+
+        // GET: api/Students/5
+        [HttpGet("{id}")]
+        //For GetStudent, it returns a single Student object with a 200 OK
+        //response when found, and a 404 Not Found
+        //response when the student with the specified ID is not found.
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Student))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Student>> GetStudent(Guid id)
+        {
+            var student = await _context.Students.FindAsync(id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return student;
+        }
+
+        // PUT: api/Students/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        //For PutStudent, it returns the updated Student object with a 200 OK response
+        //when successful, a 400 Bad Request response when the request is invalid(ID mismatch),
+        //and a 404 Not Found response when the student with the specified ID is not found.
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Student))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<Student>> PutStudent(Guid id, Student student)
+        {
+            if (id != student.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(student).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StudentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok(student);
+        }
+
+        // POST: api/Students
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        //For PostStudent, it returns the newly created Student object with a 201 Created response.
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(Student))]
+        public async Task<ActionResult<Student>> PostStudent(Student student)
+        {
+            _context.Students.Add(student);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetStudent", new { id = student.ID }, student);
+        }
+
+        // DELETE: api/Students/5
+        [HttpDelete("{id}")]
+        //For DeleteStudent, it returns a 204 No Content response when
+        //successful and a 404 Not Found response when the student with the specified ID is not found.
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> DeleteStudent(Guid id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool StudentExists(Guid id)
+        {
+            return _context.Students.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ Lab6/Data/StudentDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Lab6.Models;
+
+
+namespace Lab6.Data
+{
+    public class StudentDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {          
+            modelBuilder.Entity<Student>().HasKey(s => s.ID);
+            modelBuilder.Entity<Student>().Property(s => s.FirstName).HasMaxLength(50);
+            modelBuilder.Entity<Student>().Property(s => s.LastName).HasMaxLength(50);
+            modelBuilder.Entity<Student>().Property(s => s.Program).HasMaxLength(100);
+
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
